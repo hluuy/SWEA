c4ff34a0fbc785eee7c1788e880b95fcef146a49
hluuy <dudwns1289@gmail.com>
2023-08-10 16:02:35 +0900

    update

---
+++ 23_08_10/2005_파스칼의삼각형_구조대.py
@@ -0,0 +1,14 @@
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    MAP = [[0] * (N+1) for _ in range(N + 1)]
+    MAP[1][1] = 1
+    for i in range(2, N + 1):
+        for j in range(1, i + 1):
+            MAP[i][j] = MAP[i - 1][j - 1] + MAP[i - 1][j]
+
+    print(f'#{t}')
+    for i in range(1, N + 1):
+        for j in range(1, i + 1):
+            print(MAP[i][j], end= ' ')
+        print()
