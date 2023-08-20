d14256d64177c60998d599f211912b2486d3f55e
hluuy <dudwns1289@gmail.com>
2023-08-20 15:48:08 +0900

    update

---
+++ 23_08_20/16655_노드의거리.py
@@ -0,0 +1,31 @@
+def bfs(S, G):
+    visited = [0] * (V + 1)
+    visited[S] = 1
+    Q = []
+    Q.append(S)
+    while Q:
+        r = Q.pop(0)
+        if r == G:
+            return visited[r] -1
+        for i in stack[r]:
+            if visited[i] == 0:
+                visited[i] = visited[r] + 1
+                Q.append(i)
+    return 0
+
+T = int(input())
+for t in range(1, T + 1):
+    V, E = map(int, input().split())
+    # visited = [0] * (51)
+    stack = [[] for _ in range(V + 1)]
+    # Q = []
+    for i in range(E):
+        a, b = map(int, input().split())
+        stack[a].append(b)
+        stack[b].append(a)
+    # lst = [list(map(int, input().split()))for _ in range(E)]
+    S, G = map(int, input().split())
+
+    ans = bfs(S, G)
+
+    print(f'#{t} {ans}')
