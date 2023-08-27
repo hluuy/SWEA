c4c811ef158d91d2b542414f0c93a7c23abce208
hluuy <dudwns1289@gmail.com>
2023-08-27 18:06:15 +0900

    update

---
+++ 23_08_27/차르봄바.py
@@ -0,0 +1,19 @@
+T = int(input())
+for t in range(1, T + 1):
+    N, M = map(int, input().split())
+    MAP = [list(map(int, input().split()))for _ in range(N)]
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    max_v = 0
+    for i in range(N):
+        for j in range(N):
+            a = MAP[i][j]
+            s = 0
+            for p in range(4):
+                for q in range(1, M + 1):
+                    ni, nj = i + di[p] * q, j + dj[p] * q
+                    if 0 <= ni < N and 0 <= nj < N:
+                        s += MAP[ni][nj]
+            if max_v < s+a:
+                max_v = s+a
+    print(f'#{t} {max_v}')
